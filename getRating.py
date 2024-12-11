11149798d81471ebfdb8e9c88cd94e3160f1473d
aniketdunedar <aniketdunedar@gmail.com>
2024-12-11 20:51:21 +0530

    Add files via upload

---
+++ getRating.py
@@ -0,0 +1,16 @@
+import re
+
+def Numconvert(rate):
+    Number = {
+        'Zero':0,
+        'one':1,
+        'Two':2,
+        'Three':3,
+        'Four':4,
+        'Five':5,
+        'Six':6,
+        'Seven':7,
+        'Eight':8,
+        'Nine':9
+     }
+    return Number[rate]
